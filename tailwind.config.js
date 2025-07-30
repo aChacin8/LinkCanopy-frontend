74844f59901e779669c149bf1050348de451ad7f
mrMcAko <alechacin08@gmail.com>
2025-07-29 21:01:44 -0600

    feat: Add framework tailwind and the configuration to use it

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', 
+            './src/**/*.{js,ts,jsx,tsx}' /*Permitimos que taildwind se ejecute en todos los archivos donde sea ocupado */
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
