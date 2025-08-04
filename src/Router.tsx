a388319d52eb8a805189b03d180a8e680d0fc292
mrMcAko <alechacin08@gmail.com>
2025-08-04 17:29:03 -0600

    feat: Update Router to include admin routes and correct path structure for authentication

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,14 +2,21 @@ import { BrowserRouter, Routes, Route } from "react-router"
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import AuthLayout from "./layouts/AuthLayout";
+import AppLayout from "./layouts/AppLayout";
+import Admin from "./pages/Admin";
+import Profile from "./pages/Profile";
 
 const Router = () => {
   return ( 
     <BrowserRouter> 
       <Routes>
-        <Route element={<AuthLayout />}>  
-          <Route path= '/auth/login' element= {<Login/>}/>
-          <Route path= '/auth/register' element= {<SignUp/>}/>
+        <Route path='/auth' element={<AuthLayout />}>  
+          <Route path= 'login' element= {<Login/>}/>
+          <Route path= 'register' element= {<SignUp/>}/>
+        </Route>
+        <Route path='/admin' element = {<AppLayout/>}>
+          <Route index={true} element={<Admin/>}/>
+          <Route path="profile" element={<Profile/>}/>
         </Route>
       </Routes>
     </BrowserRouter>
