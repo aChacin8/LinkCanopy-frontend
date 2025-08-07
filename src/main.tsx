4bc83a83ebf6894c87b006b45b477a30b8d523bf
mrMcAko <alechacin08@gmail.com>
2025-08-07 17:00:26 -0600

    feat: Add Toaster component for enhanced notifications

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { Toaster } from 'sonner'
 
 import Router from './Router.tsx'
 import './index.css'
@@ -12,6 +13,7 @@ createRoot(document.getElementById('root')!).render(
       <Router />
       <ReactQueryDevtools/>
     </QueryClientProvider>
-    
+    <Toaster richColors/>
+
   </StrictMode>,
 )
