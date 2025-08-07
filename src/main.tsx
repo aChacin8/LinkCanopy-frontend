bf7c45d96940838b34fdb969c24faf94b6ede077
mrMcAko <alechacin08@gmail.com>
2025-08-06 19:41:34 -0600

    feat: Adding React Query

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
 import Router from './Router.tsx'
+import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Router />
+    <QueryClientProvider client={new QueryClient()}>
+      <Router />
+      <ReactQueryDevtools/>
+    </QueryClientProvider>
+    
   </StrictMode>,
 )
