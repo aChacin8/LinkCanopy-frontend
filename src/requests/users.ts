f1e56c5bdf1e160bb77529258e397753dcfa24e1
mrMcAko <alechacin08@gmail.com>
2025-08-07 17:02:56 -0600

    feat: Implement updateUser function for user updates

--- src/requests/users.ts
+++ src/requests/users.ts
@@ -1,11 +1,10 @@
     import { isAxiosError } from "axios"
     import api from "../config/axios"
-    import type { IUser } from "../interfaces/index"
+    import type { IUser, ProfileFormData } from "../interfaces/index"
 
     export const getUser = async () => {
         try {
             const response = await api.get <IUser> ('/user')
-
             return response.data
         } catch (error) {
             if (isAxiosError(error) && error.response){
@@ -13,4 +12,15 @@
             }
         }
 
+    }
+
+    export const updateUser = async (formData : ProfileFormData) => {
+        try {
+            const response = await api.patch <string> ('/user', formData)
+            return response.data
+        } catch (error) {
+            if (isAxiosError(error) && error.response){
+                throw new Error(error.response.data.error)
+            }
+        }
     }
