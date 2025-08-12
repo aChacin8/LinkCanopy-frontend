3dd1817e5b9818f2b0205162ddf333d1f0c954ed
mrMcAko <alechacin08@gmail.com>
2025-08-12 14:52:30 -0600

    feat: Add LinkCanopyInput component for social link management

---
+++ src/components/LinkCanopyInput.tsx
@@ -0,0 +1,38 @@
+import type { LinksCanopy } from "../interfaces"
+
+interface LinkCanopyInputProps {
+    link: LinksCanopy
+    handleUrlChange: (e : React.ChangeEvent<HTMLInputElement>) => void
+    handleEnableLink: (socialNetwork : string) => void
+}
+
+const LinkCanopyInput = ({ link, handleUrlChange, handleEnableLink }: LinkCanopyInputProps) => {
+    return (
+        <div className="bg-stone-300 shadow-sm p-3 flex items-center gap-3 rounded-md">
+            <div
+                className="w-12 h-12 bg-cover "
+                id="social_icon"
+                style={{ backgroundImage: `url('/social/icon_${link.name}.svg')` }}
+            >
+            </div>
+            <input
+                type="text"
+                value={link.url}
+                onChange={handleUrlChange}
+                name= {link.name}
+                className="bg-stone-300 flex-1 border border-stone-400 rounded-lg"
+            />
+            <label className="relative inline-flex items-center cursor-pointer">
+                <input type="checkbox" className="sr-only peer" checked={link.enabled} onChange={() => handleEnableLink(link.name)}  />
+                <div className="w-11 h-6 bg-stone-400 rounded-full peer peer-checked:bg-green-500 
+                            peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-green-300
+                            after:content-[''] after:absolute after:top-[2px] after:left-[2px]
+                            after:bg-white after:border after:border-gray-300 after:rounded-full after:h-5 after:w-5
+                            after:transition-all peer-checked:after:translate-x-full peer-checked:after:border-white">
+                </div>
+            </label>
+        </div>
+    )
+}
+
+export default LinkCanopyInput
