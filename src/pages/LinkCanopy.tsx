e1cc93668642fa01d3445497e62f4404af27d536
mrMcAko <alechacin08@gmail.com>
2025-08-12 14:52:36 -0600

    feat: Implement LinkCanopy component for managing social links

---
+++ src/pages/LinkCanopy.tsx
@@ -0,0 +1,43 @@
+import { useState } from "react"
+
+import { social } from "../data/social"
+import LinkCanopyInput from "../components/LinkCanopyInput"
+
+const LinkCanopy = () => {
+  const [linksCanopy, setLinksCanopy] = useState (social)
+
+  const handleUrlChange = (e : React.ChangeEvent<HTMLInputElement>) => {
+    const updatedLink = linksCanopy.map(link => link.name === e.target.name 
+                                          ? {...link, url: e.target.value} 
+                                          : link
+                                        ) 
+    
+    setLinksCanopy(updatedLink)
+  }
+
+  const handleEnableLink = ( socialNetwork : string) => {
+    const updatedLink = linksCanopy.map(link => link.name === socialNetwork 
+                                          ? { ...link, enabled: !link.enabled} 
+                                          : link
+                                        )
+    
+    setLinksCanopy(updatedLink)
+  }
+  
+  return ( 
+    <>
+      <div className='space-y-3'>
+        {linksCanopy.map( link => (
+          <LinkCanopyInput 
+            key={link.name} 
+            link = {link}
+            handleUrlChange = { handleUrlChange }
+            handleEnableLink={ handleEnableLink}
+          />
+        ))}
+      </div>
+    </>
+  )
+}
+
+export default LinkCanopy
