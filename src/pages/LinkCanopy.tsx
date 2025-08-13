ff232711f0fb41d4e7e7946333beee7bf941bd72
mrMcAko <alechacin08@gmail.com>
2025-08-12 20:19:54 -0600

    feat: Add save button to update user links and handle link state management

--- src/pages/LinkCanopy.tsx
+++ src/pages/LinkCanopy.tsx
@@ -1,12 +1,38 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { toast } from "sonner"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
 
 import { social } from "../data/social"
 import LinkCanopyInput from "../components/LinkCanopyInput"
 import { isValidUrl } from "../utils"
+import { updateUser } from "../requests/users"
+import type { IUser, ISocialNetwork } from "../interfaces"
 
 const LinkCanopy = () => {
   const [linksCanopy, setLinksCanopy] = useState (social)
+  const queryClient = useQueryClient()
+  const user : IUser = queryClient.getQueryData(['user'])!
+
+  const updateLinkMutation = useMutation ({
+    mutationFn: updateUser,
+    onError: (err) => {
+      toast.error (err.message)
+    },
+    onSuccess: (data) => {
+      toast.success(data)
+    }
+  })
+
+  useEffect (()=> {
+    const updatedData = linksCanopy.map ( item => {
+      const userLink = JSON.parse(user.links).find((link : ISocialNetwork )=> link.name === item.name)
+      if (userLink) {
+        return { ...item, url: userLink.url, enabled: userLink.enabled}
+      }
+      return item
+    })
+    setLinksCanopy(updatedData)
+  }, [])
 
   const handleUrlChange = (e : React.ChangeEvent<HTMLInputElement>) => {
     const updatedLink = linksCanopy.map(link => link.name === e.target.name 
@@ -15,6 +41,13 @@ const LinkCanopy = () => {
                                         ) 
     
     setLinksCanopy(updatedLink)
+
+    queryClient.setQueryData(['user'], (prevData: IUser) => {
+      return {
+        ...prevData,
+        links: JSON.stringify(updatedLink)
+      }
+    })
   }
 
   const handleEnableLink = ( socialNetwork : string) => {
@@ -30,7 +63,15 @@ const LinkCanopy = () => {
                                         })
     
     setLinksCanopy(updatedLink)
+  
+    queryClient.setQueryData(['user'], (prevData: IUser) => {
+      return {
+        ...prevData,
+        links: JSON.stringify(updatedLink)
+      }
+    })
   }
+  
   
   return ( 
     <>
@@ -43,6 +84,13 @@ const LinkCanopy = () => {
             handleEnableLink={ handleEnableLink}
           />
         ))}
+        <button
+            type="submit"
+            className="p-2 text-lg w-full bg-stone-600 text-white rounded hover:bg-stone-700"
+            onClick={() => updateLinkMutation.mutate(user)}
+          >
+            Save
+          </button>
       </div>
     </>
   )
