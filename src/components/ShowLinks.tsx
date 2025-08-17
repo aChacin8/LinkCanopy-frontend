daffb5efa6100b547e50ae5b85b0973be2e1f2a3
mrMcAko <alechacin08@gmail.com>
2025-08-16 20:19:36 -0600

    feat: Integrate sortable functionality in ShowLinks component for improved link handling

--- src/components/ShowLinks.tsx
+++ src/components/ShowLinks.tsx
@@ -1,3 +1,6 @@
+import { useSortable } from "@dnd-kit/sortable"
+import { CSS } from "@dnd-kit/utilities"
+
 import type { ISocialNetwork } from "../interfaces"
 
 interface IShowLinks {
@@ -5,8 +8,23 @@ interface IShowLinks {
 }
 
 const ShowLinks = ({ link }: IShowLinks) => {
+    const { attributes, listeners, setNodeRef, transform, transition } = useSortable ({
+        id: link.id
+    }) 
+
+    const style = {
+        transform: CSS.Transform.toString(transform),
+        transition
+    }
+
     return (
-        <li className="bg-stone-200 px-5 py-2 flex items-center gap-5 rounded-lg">
+        <li 
+            ref = { setNodeRef }
+            style={ style }
+            className="bg-stone-200 px-5 py-2 flex items-center gap-5 rounded-lg"
+            {...attributes}
+            {...listeners}
+        >
             <div
                 className="w-12 h-12 bg-cover "
                 id="social_icon"
