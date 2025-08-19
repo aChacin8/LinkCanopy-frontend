3d29c987350dcde810ce6ba6fb037035118766fb
mrMcAko <alechacin08@gmail.com>
2025-08-18 18:48:42 -0600

    feat: Creating the initials functions to Handle Page

--- src/pages/Handle.tsx
+++ src/pages/Handle.tsx
@@ -1,8 +1,30 @@
+import { useQuery } from "@tanstack/react-query";
+import { useNavigate, useParams } from "react-router";
+
+import { getUserByHandle } from "../requests/users"
+
 
 const Handle = () => {
-  return (
+
+  const params = useParams()
+  const handle = params.handle!
+
+  const { data, isLoading, isError } = useQuery({
+    queryFn: () => getUserByHandle(handle),
+    queryKey: ['handle', handle],
+    refetchOnWindowFocus: false,
+    retry: 1
+  })
+
+  const navigate = useNavigate()
+
+  if (isLoading) return <p>Loading...</p>;
+  
+  if (isError) return navigate('/error/404')
+
+  if (data) return (
     <div>
-      
+
     </div>
   )
 }
