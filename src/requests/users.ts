d1e2cd5a00a426d60a8844ab7eebf8ccbe42ae9e
mrMcAko <alechacin08@gmail.com>
2025-08-18 18:48:03 -0600

    feat: Creating async function to get user handle data

--- src/requests/users.ts
+++ src/requests/users.ts
@@ -1,41 +1,51 @@
-    import { isAxiosError } from "axios"
-    import api from "../config/axios"
-    import type { IUser } from "../interfaces/index"
+import { isAxiosError } from "axios"
+import api from "../config/axios"
+import type { IUser } from "../interfaces/index"
 
-    export const getUser = async () => {
-        try {
-            const response = await api.get <IUser> ('/user')
-            return response.data
-        } catch (error) {
-            if (isAxiosError(error) && error.response){
-                throw new Error(error.response.data.error)
-            }
-        }
-
-    }
-
-    export const updateUser = async (formData : IUser) => {
-        try {
-            const response = await api.patch <string> ('/user', formData)
-            return response.data
-        } catch (error) {
-            if (isAxiosError(error) && error.response){
-                throw new Error(error.response.data.error)
-            }
+export const getUser = async () => {
+    try {
+        const response = await api.get<IUser>('/user')
+        return response.data
+    } catch (error) {
+        if (isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error)
         }
     }
+}
 
-    export const uploadImg = async (file : File) => {
-        const formData = new FormData ()
-        formData.append('file', file)
-        
-        try {
-            const response = await api.post ('/user/img', formData)
-            return response.data
-
-        } catch (error) {
-            if (isAxiosError(error) && error.response){
-                throw new Error(error.response.data.error)
-            }
+export const updateUser = async (formData: IUser) => {
+    try {
+        const response = await api.patch<string>('/user', formData)
+        return response.data
+    } catch (error) {
+        if (isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error)
         }
     }
+}
+
+export const uploadImg = async (file: File) => {
+    const formData = new FormData()
+    formData.append('file', file)
+
+    try {
+        const response = await api.post('/user/img', formData)
+        return response.data
+
+    } catch (error) {
+        if (isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error)
+        }
+    }
+}
+
+export const getUserByHandle = async (handle : string) => {
+    try {
+        const response = await api.get(`/user/${handle}`)
+        return response.data
+    } catch (error) {
+        if (isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error)
+        }
+    }
+}
