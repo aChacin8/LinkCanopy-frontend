4397895534663470c1555d498838bde77aa53c22
mrMcAko <alechacin08@gmail.com>
2025-08-18 22:02:58 -0600

    feat: Creating the Home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import Header from "../components/Header"
+
+const Home = () => {
+  return (
+    <>
+        <Header/>
+        <div className="bg-stone-400 min-h-screen py-10">
+            
+        </div>
+    </>
+  )
+}
+
+export default Home
