1e27afe345d6d2146291d5b0d55f987438f61a4e
mrMcAko <alechacin08@gmail.com>
2025-08-18 19:57:48 -0600

    feat: Adding the handle component to the Handle Page

--- src/pages/Handle.tsx
+++ src/pages/Handle.tsx
@@ -1,32 +1,32 @@
 import { useQuery } from "@tanstack/react-query";
 import { useNavigate, useParams } from "react-router";
+import { useEffect } from "react";
 
-import { getUserByHandle } from "../requests/users"
-
+import { getUserByHandle } from "../requests/users";
+import HandleComponent from "../components/HandleComponent";
 
 const Handle = () => {
-
-  const params = useParams()
-  const handle = params.handle!
+  const params = useParams();
+  const handle = params.handle!;
 
   const { data, isLoading, isError } = useQuery({
     queryFn: () => getUserByHandle(handle),
-    queryKey: ['handle', handle],
-    refetchOnWindowFocus: false,
-    retry: 1
-  })
+    queryKey: ["handle", handle],
+    retry: 1,
+  });
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isError) {
+      navigate("/error/404");
+    }
+  }, [isError, navigate]);
 
   if (isLoading) return <p>Loading...</p>;
-  
-  if (isError) return navigate('/error/404')
 
-  if (data) return (
-    <div>
+  if (data) return <HandleComponent data={data} />;
 
-    </div>
-  )
-}
+};
 
-export default Handle
+export default Handle;
