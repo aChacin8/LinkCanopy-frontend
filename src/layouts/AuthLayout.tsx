d5f4df7a53109fe4d7b7cfdc7aba55310fd03af6
mrMcAko <alechacin08@gmail.com>
2025-08-25 18:19:22 -0600

    feat: Adding redirect to home at logo

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,12 +1,14 @@
-import { Outlet } from "react-router";
+import { NavLink, Outlet } from "react-router";
 
 const AuthLayout = () => {
     return (
         <>
-            <div className='min-h-screen bg-stone-400'>
+            <div className='min-h-screen bg-stone-300'>
                 <div className='max-w-xs mx-auto px-5 '>
-                    <img src= '/logo.png' alt='Link Canopy Link' className="p-11 mb-24"/>
-                    <Outlet/>
+                    <NavLink to={'/'}>                   
+                        <img src='/logo.png' alt='Link Canopy Logo' className="p-11 mb-24" />
+                    </NavLink>
+                    <Outlet />
                 </div>
             </div>
         </>
