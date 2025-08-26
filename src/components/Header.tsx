5ec556078a222bb9f81c405b373d82de53c62ad6
mrMcAko <alechacin08@gmail.com>
2025-08-25 18:09:44 -0600

    feat: Creating conditional to header view

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,22 @@
+import { NavLink, useLocation } from "react-router"
+
+import AdminNavigation from "./nav/AdminNavigation"
+import HomeNavigation from "./nav/HomeNavigation"
 
 const Header = () => {
+    const location = useLocation()
+
     return (
         <header className="bg-stone-700 py-4 shadow-md">
             <div className="max-w-6xl mx-auto flex flex-col md:flex-row items-center justify-between gap-4 px-4">
                 <div className="w-full lg:p-0 md:w-1/3">
-                    <img src="/logo.png" className="w-32" />
+                <NavLink to={'/'}>
+                    <img src="/logo.png" className="w-32"  />
+                </NavLink>
                 </div>
-                
-                <button
-                    onClick={() => { }}
-                    className="bg-lime-500 hover:bg-lime-600 transition-colors px-4 py-2 text-sm font-semibold text-stone-900 rounded-lg shadow"
-                >
-                    Logout
-                </button>
+                <nav className="flex gap-3 justify-center mt-4">
+                    {location.pathname === '/' ? <HomeNavigation/> : <AdminNavigation/>}
+                </nav>
             </div>
         </header>
     )
