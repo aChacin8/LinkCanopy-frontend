9892ebb91f37848b0f2a973b6bb6529a795b95ef
mrMcAko <alechacin08@gmail.com>
2025-08-27 19:21:05 -0600

    feat: Creating the logout function

--- src/components/btn/LogoutButton.tsx
+++ src/components/btn/LogoutButton.tsx
@@ -1,8 +1,21 @@
+import { useQueryClient } from "@tanstack/react-query"
+import { useNavigate } from "react-router"
+
+import { removeToken } from "../../utils/tokenStorage"
+
 
 const LogoutButton = () => {
+    const queryClient = useQueryClient()
+    const navigate = useNavigate()
+
+    const logout = () => {
+        queryClient.invalidateQueries({queryKey: ['user']})
+        removeToken()
+        navigate('/auth/login')
+    }
     return (
         <button
-            onClick={() => { }}
+            onClick={logout}
             className="bg-lime-500 hover:bg-lime-600 transition-colors px-4 py-2 text-sm font-semibold text-stone-900 rounded-lg shadow"
         >
             Logout
